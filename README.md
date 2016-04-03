689f2f72b30158c7d48359c36a87baf886d62155
rohitrango <rohit.rango@gmail.com>
2016-04-03 21:04:01 +0530

    weather Forecast script + Readme update

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 	<li>download.py - Downloads any file whose link is provided on terminal or stored in clipboard.</li>
 	<li>explosm-comic-downloader - Downloads latest comics from explosm.net. User enters number of downloads and folder name to download in.</li>
 	<li>2048-player - Opens the 2048 game and sends the UP, RIGHT, DOWN, LEFT keys. Not an effective way, but still fun :P </li>
+	<li>weatherForecast - Takes city name from user and prints the latest weather forecast by <a href="OpenWeatherMap.org">OpenWeatherMap.org</a> </li>
 </ol>
 
 <a href="https://github.com/rohitrango">My GitHub</a><br>
---
+++ weatherForecast/weatherforecast.py
@@ -0,0 +1,26 @@
+import json, requests, sys, datetime
+
+cityName = raw_input("Enter city name.\n")
+
+#Didn't give my API ID for obvious reasons.
+link = "http://api.openweathermap.org/data/2.5/weather?q=%s&APPID={YOUR_API_ID}"%(cityName)
+try:
+	data = requests.get(link)
+
+	if(data.status_code != 200):
+		print "Error "+str(data.status_code)
+		print "Closing program..."
+		sys.exit()
+	else:
+		jsondata = json.loads(data.text)
+		print "Weather Forecast \n"
+		print "City name - "+jsondata['name'] 
+		print "Last updated at - " + datetime.datetime.fromtimestamp(jsondata['dt']).strftime("%d-%m-%Y, %H:%M:%S")		
+		print "\nWeather - " + jsondata['weather'][0]['main']
+		print "Weather Description - "+jsondata['weather'][0]['description']
+		print "\nMin. temperature - " + str(jsondata['main']['temp_min']-273.15) +" Celsius."	
+		print "Max. temperature - " + str(jsondata['main']['temp_max']-273.15) + " Celsius."
+		print "Atmospheric pressure - "+str(jsondata['main']['pressure']) + " hPa" 
+		print "Humidity - " + str(jsondata['main']['humidity']) + "%\n"
+except:
+	print "Some error occured. Sorry for that."
